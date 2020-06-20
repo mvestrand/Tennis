55e2b91a32ab41fd74b451afe1fd8630a55dd52a
Max Vestrand <maxwellvest@gmail.com>
2020-06-20 16:57:26 -0600

    Data loaded into tibbles

--- setup.R
+++ setup.R
@@ -14,3 +14,41 @@ datapath <- "./data"
 if (!dir.exists(datapath)) {
   unzip(zipfile, exdir=datapath)
 }
+
+# Load the matches data
+match_files <- list.files(path = datapath, pattern="atp_matches_[0-9]*.csv")
+atp_matches = NULL
+for(file in match_files) {
+  col_types <- cols(
+    tourney_id = col_character(),
+    tourney_name = col_character(),
+    tourney_date = col_date("%Y%m%d"),
+    surface = col_character(),
+    winner_id = col_integer(),
+    loser_id = col_integer(),
+    score = col_character(),
+    best_of = col_integer(),
+    round = col_character(),
+    minutes = col_double()
+  )
+  matches_data <- read_csv(file.path(datapath, file), col_types=col_types)
+  if(is.null(atp_matches)) {
+    atp_matches <- matches_data
+  }
+  else {
+    atp_matches <- bind_rows(atp_matches, matches_data)
+  }
+}
+atp_matches <- atp_matches %>%
+  mutate(surface=factor(surface))
+         
+
+# Load the player data
+atp_players <- read_csv(file.path(datapath, "atp_players.csv"), col_types=cols(
+  player_id = col_integer(),
+  name_first = col_character(),
+  name_list = col_character(),
+  hand = col_factor(),
+  birthdate = col_date("%Y%m%d"),
+  country = col_factor()
+))
