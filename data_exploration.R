27d68c56e4323399dc886028fac13e962bd5aa6b
Max Vestrand <maxwellvest@gmail.com>
2020-06-22 18:57:55 -0600

    Some data tidying

---
+++ data_exploration.R
@@ -0,0 +1,131 @@
+########################################################
+# Explore the dataset
+#
+
+
+#==========================================
+# Tidying - Look at the raw data tables
+#   to find edge-cases and incorrect values
+#==========================================
+
+# Matches without a numeric score
+raw_matches %>% 
+  filter(!str_detect(score, "[0-9]")) %>%
+  group_by(score) %>%
+  summarize(n=n())
+
+# Matches counted as retired
+raw_matches %>%
+  filter(str_detect(score, regex("ret", ignore_case=TRUE))) %>%
+  nrow()
+
+# Look at surface values
+raw_matches %>%
+  group_by(surface) %>%
+  summarize(n=n())
+
+# Look at best_of values
+raw_matches %>%
+  group_by(best_of) %>%
+  summarize(n=n())
+
+# Look at na's per column
+map(raw_matches, ~sum(is.na(.)))
+
+# Find all rows with any na's
+na_matches <- raw_matches %>%
+  filter(surface %in% c('None', NA)
+         | is.na(w_ace+w_df+w_svpt+w_1stIn+w_1stWon+w_2ndWon+w_SvGms+w_bpSaved+w_bpFaced
+               +l_ace+l_df+l_svpt+l_1stIn+l_1stWon+l_2ndWon+l_SvGms+l_bpSaved+l_bpFaced))
+
+# NA's by year
+na_matches %>%
+  mutate(year=year(tourney_date)) %>%
+  group_by(year) %>%
+  summarize(n=n())
+
+# NA's by tourney name
+na_matches %>%
+  group_by(tourney_name) %>%
+  summarize(n=n()) %>%
+  arrange(-n)
+  
+# Unparsable scores
+score_regex <- regex(paste("^", strrep("(([0-9]+)-([0-9]+)(\\([0-9]+\\))?)?\\s*",5), "$", sep=""))
+unparsed_matches <- raw_matches %>%
+  mutate(surface=factor(surface),
+         round=factor(round)) %>%
+  filter(!surface %in% c('None', NA)
+         & !is.na(w_ace+w_df+w_svpt+w_1stIn+w_1stWon+w_2ndWon+w_SvGms+w_bpSaved+w_bpFaced
+                  +l_ace+l_df+l_svpt+l_1stIn+l_1stWon+l_2ndWon+l_SvGms+l_bpSaved+l_bpFaced)
+         & str_detect(score, "[0-9]")) %>%
+  mutate(retire = str_detect(score, regex("ret", ignore_case=TRUE))) %>%
+  mutate(score = trim(str_replace(score, regex("ret", ignore_case=TRUE), ""))) %>%
+  filter(!str_detect(score, score_regex))
+
+  
+# Tabulate player stats
+get_player_stats <- function(match_data) {
+  valid <- match_data %>% filter(str_detect(score, "[0-9]"))
+  
+  # Count meaningful wins and loses
+  win_counts <- valid %>%
+    group_by(winner_id) %>%
+    summarize(wins = n()) %>%
+    rename(player_id = 1)
+  lose_counts <- valid %>%
+    group_by(loser_id) %>%
+    summarize(loses = n()) %>%
+    rename(player_id = 1)
+  
+  # Count retires
+  retires <- valid %>%
+    filter(str_detect(score, regex("ret", ignore_case=TRUE)))
+  retired_on_counts <- retires %>%
+    group_by(winner_id) %>%
+    summarize(retired_on = n()) %>%
+    rename(player_id = 1)
+  retire_counts <- retires %>%
+    group_by(loser_id) %>%
+    summarize(retires = n()) %>%
+    rename(player_id = 1)
+  
+  # Count walkovers
+  walkover_counts <- match_data %>% filter(score %in% c("RET", "W/O", "Walkover")) %>%
+    group_by(loser_id) %>%
+    summarize(walkovers=n()) %>%
+    rename(player_id = 1)
+  
+  total_counts <- full_join(win_counts, lose_counts, by='player_id') %>%
+    left_join(retired_on_counts, by='player_id') %>%
+    left_join(retire_counts, by='player_id') %>%
+    left_join(walkover_counts, by='player_id') %>%
+    replace_na(list(wins=0, loses=0, retired_on=0, retires=0, walkovers=0)) %>%
+    mutate(matches = wins+loses)
+  
+}
+
+
+player_stats <- get_player_stats(raw_matches)
+
+# Players with n matches
+player_stats %>% ggplot(aes(matches)) +
+  geom_histogram(bins=30, color='black') +
+  scale_x_continuous(trans='log2') +
+  ggtitle('Players by Number of Matches')
+
+# Players by Win Percentage
+player_stats %>% 
+  mutate(win_percent = 100*wins/matches) %>% 
+  ggplot(aes(win_percent)) +
+  geom_histogram(bins=30, color='black') +
+  ggtitle('Players by Win Percentage')
+
+# Players by Win-Loss Ratio
+lambda <- 0
+player_stats %>% 
+  mutate(win_loss = (wins+lambda)/(loses+lambda)) %>% 
+  ggplot(aes(win_loss)) +
+  geom_histogram(bins=30, color='black') +
+  scale_x_continuous(trans='log2') +
+  ggtitle('Players by Win-Loss Ratio')
--- setup.R
+++ setup.R
@@ -6,7 +6,7 @@
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
 if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
-
+#if(!require(glue)) install.packages("glue", repos = "http://cran.us.r-project.org")
 
 # Unpack the dataset
 zipfile <- "365722_715072_bundle_archive.zip"
@@ -15,9 +15,9 @@ if (!dir.exists(datapath)) {
   unzip(zipfile, exdir=datapath)
 }
 
-# Load the matches data
+# Load the raw matches data
 match_files <- list.files(path = datapath, pattern="atp_matches_[0-9]*.csv")
-atp_matches = NULL
+raw_matches = NULL
 for(file in match_files) {
   col_types <- cols(
     tourney_id = col_character(),
@@ -32,16 +32,101 @@ for(file in match_files) {
     minutes = col_double()
   )
   matches_data <- read_csv(file.path(datapath, file), col_types=col_types)
-  if(is.null(atp_matches)) {
-    atp_matches <- matches_data
+  if(is.null(raw_matches)) {
+    raw_matches <- matches_data
   }
   else {
-    atp_matches <- bind_rows(atp_matches, matches_data)
+    raw_matches <- bind_rows(raw_matches, matches_data)
   }
 }
-atp_matches <- atp_matches %>%
-  mutate(surface=factor(surface))
+
+reformat_matches <- function(raw_match_data) {
+}
+
+# For whatever reason glue trim is not working, so
+# this is necessary
+trim <- function(x) {
+  str_replace(str_replace(x, "^\\s+", ""), "\\s+$", "")
+}
+
+valid_matches <- raw_matches %>%
+  mutate(surface=factor(surface),
+         round=factor(round)) %>%
+  filter(!surface %in% c('None', NA)
+         & !is.na(w_ace+w_df+w_svpt+w_1stIn+w_1stWon+w_2ndWon+w_SvGms+w_bpSaved+w_bpFaced
+                  +l_ace+l_df+l_svpt+l_1stIn+l_1stWon+l_2ndWon+l_SvGms+l_bpSaved+l_bpFaced)
+         & str_detect(score, "[0-9]")) %>%
+  # filter(str_detect(score, score_regex)) %>%
+  mutate(a_retired = FALSE,
+         o_retired = str_detect(score, regex("ret", ignore_case=TRUE))) %>%
+  select(-score, -winner_rank, -winner_rank_points, -loser_rank, -loser_rank_points)
+
+
+lead_columns = c("tourney_id", "tourney_name", "tourney_date", "athlete_id", "opponent_id", "won", "winner_id", "surface", "best_of", "round", "minutes")
+
+# Make information about who won easily removable and add missing info
+reformatted_matches <- valid_matches %>%
+  rename(athlete_id="winner_id",
+         opponent_id="loser_id",
+         a_ace="w_ace",
+         o_ace="l_ace",
+         a_df="w_df",
+         o_df="l_df",
+         a_svpt="w_svpt",
+         o_svpt="l_svpt",
+         a_sv1stIn="w_1stIn",
+         o_sv1stIn="l_1stIn",
+         a_sv1stWon="w_1stWon",
+         o_sv1stWon="l_1stWon",
+         a_sv2ndWon="w_2ndWon",
+         o_sv2ndWon="l_2ndWon",
+         a_SvGms="w_SvGms",
+         o_SvGms="l_SvGms",
+         a_svbpSaved="w_bpSaved",
+         o_svbpSaved="l_bpSaved",
+         a_svbpFaced="w_bpFaced",
+         o_svbpFaced="l_bpFaced") %>%
+  mutate(won=TRUE, winner_id=athlete_id) %>%
+  mutate(a_frac_sv1stIn = a_sv1stIn / a_svpt,
+         a_frac_sv1stWon = a_sv1stWon / a_sv1stIn,
+         a_frac_sv2ndWon = a_sv2ndWon / (a_svpt - a_sv1stIn),
+         a_frac_svbpSaved = a_svbpSaved / a_svbpFaced,
+         o_frac_sv1stIn = o_sv1stIn / o_svpt,
+         o_frac_sv1stWon = o_sv1stWon / o_sv1stIn,
+         o_frac_sv2ndWon = o_sv2ndWon / (o_svpt - o_sv1stIn),
+         o_frac_svbpSaved = o_svbpSaved / o_svbpFaced,
+         a_frac_rt1stWon = 1 - o_frac_sv1stWon,
+         a_frac_rt2ndWon = 1 - o_frac_sv2ndWon,
+         a_frac_rtbpConv = 1 - o_frac_svbpSaved,
+         o_frac_rt1stWon = 1 - a_frac_sv1stWon,
+         o_frac_rt2ndWon = 1 - a_frac_sv2ndWon,
+         o_frac_rtbpConv = 1 - a_frac_svbpSaved,
+         a_svptWon = a_sv1stWon+a_sv2ndWon,
+         a_frac_svptWon = (a_svptWon) / a_svpt,
+         o_svptWon = o_sv1stWon+o_sv2ndWon,
+         o_frac_svptWon = (o_svptWon) / o_svpt,
+         a_rtptWon = o_svpt - o_svptWon,
+         a_frac_rtptWon = a_rtptWon / o_svpt,
+         o_rtptWon = a_svpt - a_svptWon,
+         o_frac_rtptWon = o_rtptWon / a_svpt,
+         a_ptWon = a_svptWon + a_rtptWon,
+         o_ptWon = o_svptWon + o_rtptWon,
+         a_frac_ptWon = a_ptWon / (a_ptWon + o_ptWon),
+         o_frac_ptWon = o_ptWon / (a_ptWon + o_ptWon)) %>%
+  select(noquote(append(lead_columns, sort(setdiff(colnames(.), lead_columns)))))
+
+reversed_matches <- reformatted_matches %>%
+
+colnames(reformatted_matches)   
+
+#.[,c(1,2,3)]
+#reordered_matches <- reformatted_matches[,c(1:3,5,6,30,31,4,7:9,28,10:18,46,50,54,32:35,40,41,)]         
+                  
          
+                 
+
+# Reformat the matches data for usability and remove incomplete data
+
 
 # Load the player data
 atp_players <- read_csv(file.path(datapath, "atp_players.csv"), col_types=cols(
@@ -52,3 +137,17 @@ atp_players <- read_csv(file.path(datapath, "atp_players.csv"), col_types=cols(
   birthdate = col_date("%Y%m%d"),
   country = col_factor()
 ))
+
+# Split into training and test sets
+matches_final_train <- atp_matches %>%
+  filter(tourney_date < "2018-01-01")
+
+matches_final_test <- atp_matches %>%
+  filter(tourney_date >= "2018-01-01")
+
+matches_train <- matches_final_train %>%
+  filter(tourney_date < "2017-01-01")
+
+matches_test <- matches_final_train %>%
+  filter(tourney_date >= "2017-01-01")
+
